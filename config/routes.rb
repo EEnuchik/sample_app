4633c599e742dc8ef81fab2fd88c853c7719058e
JonyLogy <jonylogy@gmail.com>
2013-12-02 23:41:31 +0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
 
   get "static_pages/home"
 
   get "static_pages/help"
+
+  get "static_pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
