bd9e3f76fdb1d02dd3dbc64cde0336c7da14a1bd
EEnuchik <enuchik@gmail.com>
2014-01-14 19:51:16 +0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      			   "Jony"
+    email     			   "jonylogy@gmail.com"
+    password               "34jony56"
+    password_confirmation   "34jony56"
+  end
+end
